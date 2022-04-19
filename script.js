801cb8fe9f5f645e625e8910953667ab5c2be5ef
Matthew <matthewtoro17@gmail.com>
2022-04-19 19:37:46 +0800

    Add functionality for adding books to library

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let myLibrary = [];
 
 const cardContainer = document.querySelector('.card-container');
-const newBookBtn = document.querySelector('.new-book-btn');
-const closeBtn = document.querySelector('.close-btn');
 const formWrapper = document.querySelector('.form-wrapper');
+
+const newBookBtn = document.querySelector('.new-book-btn');
+const addBtn = document.querySelector('.form-btn');
+const closeBtn = document.querySelector('.close-btn');
+
+const addBookForm = document.querySelector('.add-book-form');
 
 function Book(title, author, numPages, isRead) {
   this.title = title;
@@ -16,56 +20,78 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
+function addBookToDOM(book) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  const title = document.createElement('h2');
+  title.classList.add('title');
+  title.textContent = book.title;
+  card.appendChild(title);
+
+  const author = document.createElement('h4');
+  author.classList.add('author');
+  author.textContent = book.author;
+  card.appendChild(author);
+
+  const numPages = document.createElement('div');
+  numPages.classList.add('num-pages');
+  numPages.textContent = book.numPages;
+  card.appendChild(numPages);
+
+  const isRead = document.createElement('div');
+  isRead.classList.add('is-read');
+  isRead.textContent = (book.isRead) ? 'Read!' : 'Not read.';
+  card.appendChild(isRead);
+
+  cardContainer.appendChild(card);
+}
+
 function displayBooks() {
+  while (cardContainer.firstChild) {
+    cardContainer.removeChild(cardContainer.firstChild);
+  }
+
   myLibrary.forEach((book) => {
-    const card = document.createElement('div');
-    card.classList.add('card');
-
-    const title = document.createElement('h2');
-    title.classList.add('title');
-    title.textContent = book.title;
-    card.appendChild(title);
-
-    const author = document.createElement('h4');
-    author.classList.add('author');
-    author.textContent = book.author;
-    card.appendChild(author);
-
-    const numPages = document.createElement('div');
-    numPages.classList.add('num-pages');
-    numPages.textContent = book.numPages;
-    card.appendChild(numPages);
-
-    const isRead = document.createElement('div');
-    isRead.classList.add('is-read');
-    isRead.textContent = (book.isRead) ? 'Read!' : 'Not read.';
-    card.appendChild(isRead);
-
-    cardContainer.appendChild(card);
+    addBookToDOM(book);
   });
 }
 
 function displayNewBookForm() {
   formWrapper.style.display = 'flex';
-  console.log('test');
 }
 
 function closeForm() {
   formWrapper.style.display = 'none';
 }
 
+function getBookFromForm() {
+  let title = addBookForm.elements[0].value;
+  let author = addBookForm.elements[1].value;
+  let numPages = addBookForm.elements[2].value;
+  let isRead = document.querySelector('.form-checkbox').checked;
+  
+  if (title === '') title = "Unknown Title";
+  if (author === '') author = "Unknown Author";
+  if (numPages === '') numPages = "Unknown Page Count";
+
+  return new Book(title, author, numPages, isRead);
+}
+
 newBookBtn.addEventListener('click', displayNewBookForm);
+
+addBtn.addEventListener('click', () => {
+  const book = getBookFromForm();
+
+  addBookToLibrary(book);
+  displayBooks();
+
+  addBookForm.reset();
+  closeForm();
+});
+
 closeBtn.addEventListener('click', closeForm);
+
 formWrapper.addEventListener('click', (e) => {
   if (e.target == formWrapper) closeForm();
-})
-
-const book1 = new Book("Test Book", "John E. Doe", 365, true);
-const book2 = new Book("Best Took2", "Dohn E. Joe", 300, false);
-const book3 = new Book("Test Book3", "John E. Doe", 365, true);
-
-addBookToLibrary(book1);
-addBookToLibrary(book2);
-addBookToLibrary(book3);
-
-displayBooks();
+});
